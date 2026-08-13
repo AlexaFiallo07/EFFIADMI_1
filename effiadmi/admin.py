717ce8bc87cce96460dafb894e6b2da0a6f900e0
juliana601 <jv831145@gmail.com>
2026-08-13 15:28:52 -0500

    Actualización del panel de administración

--- effiadmi/admin.py
+++ effiadmi/admin.py
@@ -28,3 +28,44 @@ class UsuarioAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('fecha_registro',)
 
+@admin.register(Inventario)
+class InventarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre_producto', 'stock_actual', 'stock_minimo', 'precio_venta', 'fecha_creacion')
+    list_filter = ('fecha_creacion',)
+    search_fields = ('nombre_producto',)
+
+@admin.register(facturas)
+class FacturasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente', 'fecha_emision', 'total')
+    list_filter = ('fecha_emision',)
+    search_fields = ('cliente__nombre',)
+
+@admin.register(notificaciones)
+class NotificacionesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mensaje', 'fecha_creacion', 'leido')
+    list_filter = ('leido', 'fecha_creacion')
+    search_fields = ('mensaje',)
+
+@admin.register(pedidos)
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente', 'fecha_pedido', 'total')
+    list_filter = ('fecha_pedido',)
+    search_fields = ('cliente__nombre',)
+
+@admin.register(productos)
+class ProductosAdmin(admin.ModelAdmin):
+    list_display = ('nombre_producto', 'precio_venta', 'precio_compra', 'stock_actual')
+    list_filter = ('stock_actual',)
+    search_fields = ('nombre_producto',)
+
+@admin.register(proveedores)
+class ProveedoresAdmin(admin.ModelAdmin):
+    list_display = ('nombre_proveedor', 'correo', 'telefono', 'direccion')
+    search_fields = ('nombre_proveedor', 'correo')
+
+@admin.register(reportes)
+class ReportesAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_creacion')
+    list_filter = ('fecha_creacion',)
+    search_fields = ('titulo',)
+
